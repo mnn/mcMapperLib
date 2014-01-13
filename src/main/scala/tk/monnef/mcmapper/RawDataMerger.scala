082166cf039abc8af66d545e72597203d107f6e8
moen <moen@366.hopto.org>
2014-01-13 10:32:30 +0100

    Implemented srg processing.

--- src/main/scala/tk/monnef/mcmapper/RawDataMerger.scala
+++ src/main/scala/tk/monnef/mcmapper/RawDataMerger.scala
@@ -3,7 +3,6 @@ package tk.monnef.mcmapper
 import tk.monnef.mcmapper.MappingSide._
 
 object RawDataMerger {
-
   def merge(srgRaw: List[List[String]], fieldsRaw: List[List[String]], methodsRaw: List[List[String]]): MappingDatabaseSimple = {
     var classMapping = new MappingSet[ClassMapping]()
     var fieldMapping = new MappingSet[FieldMapping]()
@@ -12,6 +11,7 @@ object RawDataMerger {
     // srg -> classes data, skeletons of fields and methods
     // skeletons + (mappings of fields and methods) -> whole data
 
+    // srg processing
     for {
       item <- srgRaw
       if item.length >= 3 && item.head.length > 1
@@ -21,18 +21,34 @@ object RawDataMerger {
         case "PK" => // nothing
         case "CL" =>
           val side =
-            if (item.length == 4) item(3) match {
-              case "#C" => CLIENT
-              case "#S" => SERVER
-              case _ => throw new McMapperException(s"Illegal side: '${item(3)}'")
-            }
+            if (item.length == 4) getSide(item(3))
             else BOTH
           classMapping += ClassMapping(item(1), item(2), side)
-        case "FD" => //todo
-        case "MD" => //todo
+
+        case "FD" =>
+          val side =
+            if (item.length == 4) getSide(item(3))
+            else BOTH
+          fieldMapping += FieldMapping(item(1), item(2), "", "", side)
+
+        case "MD" =>
+          val side =
+            if (item.length == 6) getSide(item(5))
+            else BOTH
+          methodMapping += MethodMapping(item(1), item(3), "", item(2), item(4), "", side)
       }
     }
 
+    // CSV processing
+    // todo
+
     MappingDatabaseSimple(classMapping, methodMapping, fieldMapping)
   }
+
+  private def getSide(in: String): MappingSide =
+    in match {
+      case "#C" => CLIENT
+      case "#S" => SERVER
+      case _ => throw new McMapperException(s"Illegal side: '$in'")
+    }
 }
--- test/main/scala/tk/monnef/mcmapper/test/RawDataMergerTests.scala
+++ test/main/scala/tk/monnef/mcmapper/test/RawDataMergerTests.scala
@@ -1,14 +1,20 @@
 package tk.monnef.mcmapper.test
 
 import org.scalatest.{FlatSpec, Matchers}
-import tk.monnef.mcmapper.{MappingObject, ClassMapping, RawDataMerger}
+import tk.monnef.mcmapper._
+import tk.monnef.mcmapper.ClassMapping
+import tk.monnef.mcmapper.FieldMapping
 
 class RawDataMergerTests extends FlatSpec with Matchers {
 
   import TestHelper._
   import tk.monnef.mcmapper.MappingSide._
 
-  def o(a: MappingObject, b: MappingObject) = a.full < b.full
+  def o(a: MappingObject, b: MappingObject) = a.obf < b.obf
+
+  def removeCommentAndFullName(in: FieldMapping): FieldMapping = in.copy(comment = "", full = "")
+
+  def removeCommentAndFullName(in: MethodMapping): MethodMapping = in.copy(comment = "", full = "")
 
   "RawDataMerger" should "construct classes" in {
     val r = RawDataMerger.merge(
@@ -20,5 +26,30 @@ class RawDataMergerTests extends FlatSpec with Matchers {
     val c = r.classes
     c.size shouldBe 2
     c.toList.sortWith(o) shouldEqual List(ClassMapping("a", "x/y", BOTH), ClassMapping("b", "q/w", CLIENT)).sortWith(o)
+  }
+
+  it should "construct partially fields" in {
+    val r = RawDataMerger.merge(
+      List(
+        List("FD:", "w/t", "n/m/f0", "#C"),
+        List("FD:", "q/t", "n/m/f1")
+      ), List(), List()
+    )
+    val c = r.fields
+    c.size shouldBe 2
+    c.toList.sortWith(o).map(removeCommentAndFullName) shouldEqual List(FieldMapping("w/t", "n/m/f0", "", "", CLIENT), FieldMapping("q/t", "n/m/f1", "", "", BOTH)).sortWith(o)
+  }
+
+  it should "construct partially methods" in {
+    val r = RawDataMerger.merge(
+      List(
+        List("MD:", "w/t", "(ZLlx;)Z", "n/m/func_0", "(ZLw/t/c0;)Z", "#C"),
+        List("MD:", "q/t", "(ZLq;)V", "n/m/func_1", "(ZLw/t/c1;)V")
+      ), List(), List()
+    )
+    val c = r.methods
+    c.size shouldBe 2
+    val expected = List(MethodMapping("w/t", "n/m/func_0", "", "(ZLlx;)Z", "(ZLw/t/c0;)Z", "", CLIENT), MethodMapping("q/t", "n/m/func_1", "", "(ZLq;)V", "(ZLw/t/c1;)V", "", BOTH))
+    c.toList.sortWith(o).map(removeCommentAndFullName) shouldEqual expected.sortWith(o)
   }
 }
